82f7f3d765e66e10fd648eb096ff17e6795c01b7
divyam kumar <divyamku8051@gmail.com>
2023-06-14 13:05:17 +0530

    Changing in diff file

--- diff.java
+++ diff.java
@@ -2,6 +2,6 @@ public class diff {
     public static void main(String[] args) {
         int a=10;
         int b=4;
-        System.out.println(a-b);
+        System.out.println("Difference is: "+(a-b));
     }
 }
